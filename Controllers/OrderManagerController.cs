e3eed3fcf2bcf3fca803957040b58f4b496644eb
Kagucuti <109417871+Kagucuti@users.noreply.github.com>
2023-11-10 15:16:57 +0200

    Initial commit

---
+++ Controllers/OrderManagerController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OrderManager.Application.Services;
+using OrderManager.Domain.Data_Transfer_Objects;
+using OrderManager.Domain.Models;
+
+namespace OrderManageAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderManagerController : ControllerBase
+    {
+        private readonly IOrderManagerService _service;
+        public OrderManagerController(IOrderManagerService service)
+        {
+            _service = service;
+        }
+        [HttpGet]
+        public async Task<IEnumerable<OrderDTO>> GetAll()
+         => await _service.GetAllOrdersAsync();
+
+        [HttpGet("{Id}")]
+        public async Task<ActionResult<OrderDTO>> Get(int Id)
+            => Ok(await _service.GetSingleOrderAsync(Id));
+        [HttpPost]
+        public async Task Add([FromBody] OrderDTO orderDTO)
+            => await _service.AddOrderAsync(orderDTO);
+        [HttpDelete("Id")]
+        public async Task Delete(int Id)
+            => await _service.DeleteOrderAsync(Id);
+        [HttpPut]
+        public async Task Uppdate(OrderDTO orderDTO)
+            => await _service.UpdateOrderAsync(orderDTO);
+        [HttpDelete("orders/{orderId}/orderlines/{orderLineId}")]
+        public async Task<IActionResult> RemoveOrderLine(int orderId, int orderLineId)
+        {
+            try
+            {
+                await _service.RemoveOrderLineAsync(orderId, orderLineId);
+
+                return Ok("OrderLine removed successfully.");
+            }
+            catch (Exception ex) {
+                return BadRequest($"Error: {ex.Message}");
+            }
+        }
+		[HttpPost("AddOrderLine/{orderId}")]
+		
+		public async Task AddOrderLine(int orderId, [FromBody]  OrderLineDTO orderLineDTO)
+		{
+			await _service.AddOrderLineAsync(orderId, orderLineDTO);
+		}
+	}
+}
---
+++ Migrations/20231110095941_InitialCreate.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OrderManager.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreateDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    ClientName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AdditionalInfo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OrderPrice = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrderLines",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Product = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<double>(type: "float", nullable: false),
+                    OrderId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderLines", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_OrderLines_Orders_OrderId",
+                        column: x => x.OrderId,
+                        principalTable: "Orders",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderLines_OrderId",
+                table: "OrderLines",
+                column: "OrderId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrderLines");
+
+            migrationBuilder.DropTable(
+                name: "Orders");
+        }
+    }
+}
